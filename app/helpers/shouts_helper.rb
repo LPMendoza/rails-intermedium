73b0299d72d0ace2397414914c19821f275eb996
LPMendoza <lpedromendoza333@gmail.com>
2021-08-09 19:43:11 -0500

    Avata helper added

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,9 +1,4 @@
 module ShoutsHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-    image_tag gravatar_url
-  end
 
   def like_button(shout)
     if current_user.has_liked?(shout)
@@ -11,6 +6,10 @@ module ShoutsHelper
     else
       link_to "Like", like_shout_path(shout), method: :post
     end
+  end
+
+  def autolink(text)
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
   end
 
 end
